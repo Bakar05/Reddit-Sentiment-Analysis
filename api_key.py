e4cc7ba7ab0bbf1c293bcff703162c38bc774848
Abu Bakar Imran <abubakar5feb2005@gmail.com>
2025-09-07 03:42:29 -0700

    Update api_key.py

--- api_key.py
+++ api_key.py
@@ -2,7 +2,8 @@ import praw
 
 # Initialize Reddit instance (configure with your credentials)
 reddit = praw.Reddit(
-    client_id="MtzMdX8CmzABOFlREgDd8Q",
-    client_secret="LvhWsJD7y7lR-7TaH3CI0lxAYkY-lg",
-    user_agent="Abu bakar"
+    client_id="",
+    client_secret="",
+    user_agent=""
+
 )
